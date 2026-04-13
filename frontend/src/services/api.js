e2d71597fd1e240fd48c6b0a20d345c74ce705aa
Mithelesh-45 <mitheleshk3005@gmail.com>
2026-04-13 18:27:14 +0530

    updating base url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: 'https://agenitc-cx.onrender.com',
 });
 
 export const sendMessage = async (message, userId = 'user_1') => {
